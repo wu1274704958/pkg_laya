59d3233bb9bbb70aeb4492e83b3ae80adf6d19bf
wusj7856 <wusj@realbull.net>
2020-08-11 14:57:15 +0800

    input ..

--- app/src/main/java/com/huolong/hf/utils/InputMethodUtils.java
+++ app/src/main/java/com/huolong/hf/utils/InputMethodUtils.java
@@ -17,7 +17,8 @@ public class InputMethodUtils {
     public static void showOrHide(Context context, View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         //  imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);//SHOW_FORCED表示强制显示
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0); //强制隐藏键盘
+        if(imm != null)
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0); //强制隐藏键盘
     }
 
     //调用隐藏系统默认的输入法
@@ -28,13 +29,33 @@ public class InputMethodUtils {
     }
 
     public static void show(Context context, View view) {
-        ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE))
-                .showSoftInput(view,InputMethodManager.SHOW_FORCED);
+        InputMethodManager imm =  ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE));
+        if(imm != null)
+            imm.showSoftInput(view,InputMethodManager.SHOW_FORCED);
     }
 
     //获取输入法打开的状态
     public static boolean isShowing(Context context) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        return imm.isActive();//isOpen若返回true，则表示输入法打开
+        if(imm != null)
+            return imm.isActive();//isOpen若返回true，则表示输入法打开
+        else
+            return  false;
+    }
+
+    public static boolean isShowing(Context context,View v) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(imm != null)
+            return imm.isActive(v);//isOpen若返回true，则表示输入法打开
+        else
+            return  false;
+    }
+
+    public static boolean isFullscreenMode(Context context) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(imm != null)
+            return imm.isFullscreenMode();//isOpen若返回true，则表示输入法打开
+        else
+            return  false;
     }
 }
