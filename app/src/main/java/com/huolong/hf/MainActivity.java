2181041ef8dfa96b28a9c894bd5c983f1f0533bb
wusj7856 <wusj@realbull.net>
2020-08-14 14:53:58 +0800

    WVCM .

--- app/src/main/java/com/huolong/hf/MainActivity.java
+++ app/src/main/java/com/huolong/hf/MainActivity.java
@@ -320,7 +320,7 @@ public class MainActivity extends Activity {
             }else
             if(consoleMessage.messageLevel() == ConsoleMessage.MessageLevel.ERROR)
             {
-                Log.e("WVCM",consoleMessage.message());
+                Log.e("WVCM",consoleMessage.message() + " " + consoleMessage.sourceId() + "   " +  consoleMessage.lineNumber());
             }
             return true;
         }
